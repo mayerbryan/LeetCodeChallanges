e4d95cabf6569761aee5d9bf03ff00987fe50690
Bryan Mayer <mayerbryan@mail.com>
2023-06-15 19:54:01 +0000

    update

--- ValidParentheses/ValidParentheses.cs
+++ ValidParentheses/ValidParentheses.cs
@@ -10,7 +10,7 @@ namespace LeetCodeChallanges.ValidParentheses
         public bool IsValid(string s) {
             while(s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
             {
-            s=s.Replace("()","").Replace("[]","").Replace("{}","");
+                s=s.Replace("()","").Replace("[]","").Replace("{}","");
             }
             return s.Length == 0;
             
